a1d90db8c4c48fca324a0dd2197a32a824cbbdbd
astern3 <avram.stern@digitas.com>
2021-02-07 14:01:07 -0600

    1.4: course information step4 - solution

--- course-info-app/src/Total.js
+++ course-info-app/src/Total.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Total(props) {
-  console.log(props);
   return (
     <div>
       <p>
         Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
       </p>
     </div>
   );
