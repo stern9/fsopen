4efc9e23d779e25c70265f5558afa4b1302d64ad
astern3 <avram.stern@digitas.com>
2021-02-08 19:33:59 -0600

    Complex state - part1d example

---
+++ part1d/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import History from "./History";
+import Button from "./Button";
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
+
+export default App;
