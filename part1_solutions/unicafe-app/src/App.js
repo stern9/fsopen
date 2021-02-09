0b7614194c68200495beff2f61ae73813dac38a1
astern3 <avram.stern@digitas.com>
2021-02-09 17:49:02 -0600

    1.7: unicafe step2 - solution

--- part1_solutions/unicafe-app/src/App.js
+++ part1_solutions/unicafe-app/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./Header";
+import Button from "./Button";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -7,7 +9,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div className="App">code here</div>;
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
+  const addAll = good + neutral + bad;
+  const averageScore = (good * 1 + neutral * 0 + bad * -1) / addAll;
+  const positivePercentage = (good / addAll) * 100;
+
+  return (
+    <div className="App">
+      <Header />
+      <Button handleClick={increaseGood} text="good" />
+      <Button handleClick={increaseNeutral} text="neutral" />
+      <Button handleClick={increaseBad} text="bad" />
+      <div>
+        <h1>Statistics</h1>
+      </div>
+      <div>good: {good}</div>
+      <div>neutral: {neutral}</div>
+      <div>bad: {bad}</div>
+      <div>all: {addAll}</div>
+      <div>average: {averageScore}</div>
+      <div>positive: {positivePercentage} %</div>
+    </div>
+  );
 };
 
 export default App;
