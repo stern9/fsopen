865eed1c90a3abb7e7f9afeef2dfe486c8180d90
astern3 <avram.stern@digitas.com>
2021-02-18 17:23:31 -0600

    1.13*: anecdotes step2 - solution

--- part1_solutions/anecdotes-app/src/App.js
+++ part1_solutions/anecdotes-app/src/App.js
@@ -12,17 +12,30 @@ const App = (props) => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
   ];
 
+  const points = new Array(anecdotes.length);
+  for (let i = 0; i < anecdotes.length; ++i) points[i] = 0;
+
   const [selected, setSelected] = useState(0);
-  const randomNumber = () => {
+  const [votes, setVotes] = useState(points);
+
+  const randomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   };
-
   // console.log(selected);
+
+  const addVotes = () => {
+    const pointsCopy = [...votes];
+    pointsCopy[selected] += 1;
+    setVotes(pointsCopy);
+    console.log(pointsCopy);
+  };
 
   return (
     <div>
       <div>{anecdotes[selected]}</div>
-      <Button handleClick={randomNumber} text="random anecdote" />
+      <div>has {votes[selected]} votes</div>
+      <Button handleClick={addVotes} text="vote" />
+      <Button handleClick={randomAnecdote} text="random anecdote" />
     </div>
   );
 };
