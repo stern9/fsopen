10d2dce1707552f7c0640f252b4124d6994c3397
astern3 <avram.stern@digitas.com>
2021-02-18 18:13:01 -0600

    1.14*: anecdotes step3 - solution

--- part1_solutions/anecdotes-app/src/App.js
+++ part1_solutions/anecdotes-app/src/App.js
@@ -12,10 +12,11 @@ const App = (props) => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
   ];
 
+  const [selected, setSelected] = useState(0);
+
   const points = new Array(anecdotes.length);
   for (let i = 0; i < anecdotes.length; ++i) points[i] = 0;
 
-  const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(points);
 
   const randomAnecdote = () => {
@@ -27,15 +28,25 @@ const App = (props) => {
     const pointsCopy = [...votes];
     pointsCopy[selected] += 1;
     setVotes(pointsCopy);
-    console.log(pointsCopy);
+  };
+
+  // console.log(votes);
+
+  const findMaxIndex = () => {
+    const maxIndex = votes.indexOf(Math.max.apply(Math, votes));
+    return maxIndex;
   };
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
+      <h1>Anecdote of the day</h1>
+      <div>"{anecdotes[selected]}"</div>
       <div>has {votes[selected]} votes</div>
       <Button handleClick={addVotes} text="vote" />
       <Button handleClick={randomAnecdote} text="random anecdote" />
+      <h1>Anecdote with more votes</h1>
+      <div>"{anecdotes[findMaxIndex()]}"</div>
+      <div>With {votes[findMaxIndex()]} votes</div>
     </div>
   );
 };
