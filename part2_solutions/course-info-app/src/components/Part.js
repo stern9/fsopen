4fe5928f67ce9263141ebcf202ab33029d1e2acd
stern9 <stern9@gmail.com>
2021-02-22 19:27:45 -0600

    2.1: Course information step6 - solution

---
+++ part2_solutions/course-info-app/src/components/Part.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercise}
+      </p>
+    </div>
+  );
+};
+
+export default Part;
