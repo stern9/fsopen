e0234ccb0abfbbd3f34b8f424deb8d792d0bfce4
stern9 <stern9@gmail.com>
2021-03-08 20:13:55 -0600

    2.10: The Phonebook Step5 - solution

--- part2_solutions/phonebook-app/src/App.js
+++ part2_solutions/phonebook-app/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -60,23 +63,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFindPerson} />
-      </div>
+      <Filter value={filter} onChange={handleFindPerson} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        onSubmit={addPerson}
+        name={{ value: newName, onChange: handleNameChange }}
+        number={{ value: newNumber, onChange: handleNumberChange }}
+      />
       <h2>Numbers</h2>
-      {rowNames()}
+      <Persons persons={rowNames()} />
     </div>
   );
 };
---
+++ part2_solutions/phonebook-app/src/components/Persons.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Persons = ({ persons }) => {
+  return <div>{persons}</div>;
+};
+
+export default Persons;
